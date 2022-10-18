1791374a75f59941cef611a6e03f591a311be84b
Andy <alipinski@mail.com>
2022-10-18 09:38:09 +0200

    header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,31 @@
 import './Header.css';
+import logo from '../../assets/logo.png';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className='container  --flex-between'>
+        <div className='logo'>
+          <img src={logo} alt='logo' width={100} />
+        </div>
+        <nav className='--flex-between'>
+          <ul>
+            <li>
+              <a href='#'>Home</a>
+              <a href='#'>About</a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <span className='toggle-btn'>
+            <BsFillMoonFill size={30} color='pink' />
+            <BsFillSunFill size={30} color='yellow' />
+            <div className='ball'></div>
+          </span>
+        </div>
+      </div>
+    </header>
   );
 };
 
