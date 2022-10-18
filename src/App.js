d9be6b86b44a0061d44797c119a7fb8ec656019f
Andy <alipinski@mail.com>
2022-10-18 07:52:59 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Hero from './components/hero/Hero';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,11 @@
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hero</h1>
+    </div>
+  );
+};
+
+export default Hero;
